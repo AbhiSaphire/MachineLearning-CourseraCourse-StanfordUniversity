93b5e117b7a379b7feb77a3ddff3aac2188e1945
Abhishek Kumar <abhishekkmr2000@gmail.com>
2020-02-13 14:59:11 +0530

    Update gradientDescentMulti.m

--- Week-2/Exercise-1/gradientDescentMulti.m
+++ Week-2/Exercise-1/gradientDescentMulti.m
@@ -16,8 +16,11 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
-
+h = X * theta;
+for i = 1:size(X,2)
+    val = (h - y) .* X(:, i);
+    theta(i) = theta(i) - (alpha * (1/m) * sum(val));
+end
 
 
 
