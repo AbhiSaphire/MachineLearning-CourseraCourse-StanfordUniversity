4415a17a7445207b58396b08d4e2f4ff570aa167
Abhishek Kumar <abhishekkmr2000@gmail.com>
2020-02-13 14:58:25 +0530

    Update gradientDescent.m

--- Week-2/Exercise-1/gradientDescent.m
+++ Week-2/Exercise-1/gradientDescent.m
@@ -6,7 +6,6 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 % Initialize some useful values
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
-
 for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
@@ -15,10 +14,13 @@ for iter = 1:num_iters
     %
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
-    %
+   
 
-
-
+    temp = X * theta;
+    for i = 1:2
+        col1 = (temp-y) .* X(:,i);
+        theta(i) = theta(i) - (alpha * sum(col1) * (1/m)) ;
+    end
 
 
 
