e52c79bbb44ff60e65c1547fe90af674c058d384
Abhishek Kumar <abhishekkmr2000@gmail.com>
2020-02-13 14:57:45 +0530

    Update computeCostMulti.m

--- Week-2/Exercise-1/computeCostMulti.m
+++ Week-2/Exercise-1/computeCostMulti.m
@@ -13,6 +13,9 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+h = X * theta;
+temp = (h - y).^2;
+J = 1/(2*m) * sum(temp);
 
 
 
