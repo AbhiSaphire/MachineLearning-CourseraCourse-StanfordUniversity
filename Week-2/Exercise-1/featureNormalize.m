7fbfbc803c363877caff27d09529f24d6497a0fa
Abhishek Kumar <abhishekkmr2000@gmail.com>
2020-02-13 15:01:56 +0530

    Update featureNormalize.m

--- Week-2/Exercise-1/featureNormalize.m
+++ Week-2/Exercise-1/featureNormalize.m
@@ -26,8 +26,12 @@ sigma = zeros(1, size(X, 2));
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
 
+mu = mean(X);
+sigma = std(X);
 
-
+for i = 1: size(X,2)
+    X_norm(:, i) = (X(:, i) - mu(:, i)) / sigma(:, i);
+end
 
 
 
