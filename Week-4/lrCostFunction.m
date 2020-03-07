b3c34dc69100e9c4f893ede620cc381e5524aa49
Abhishek Kumar <abhishekkmr2000@gmail.com>
2020-03-07 15:20:56 +0530

    Update lrCostFunction.m

--- Week-4/lrCostFunction.m
+++ Week-4/lrCostFunction.m
@@ -36,13 +36,13 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+h = sigmoid(X * theta);
 
+J = (1/m) * sum((-y' * log(h)) - ((1-y)' * log(1 - h))) + (lambda/(2*m) * sum(theta(2:end) .^ 2));
 
+grad(1) = (1/m) * (X(:, 1)' * (h - y));
 
-
-
-
-
+grad(2:end) = (1/m) * (X(:, 2:end)' * (h - y)) + (lambda/m * theta(2:end));
 
 
 % =============================================================
